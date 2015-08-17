90f99297e490c54128a20757223d244e1eb916d0
mmaiatico <maiatico@gmail.com>
2015-08-17 14:53:54 -0400

    Search demo table

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,10 @@
 class SearchController < ApplicationController
 
 	def index
+		params[:query] ||= ""
+		query = "#{params[:query].downcase}%"
 		@states = State.paginate(page: params[:page] || 1, per_page: 10)
-	end
-
-	def search
-
+							.where("LOWER(name) LIKE ? OR LOWER(code) LIKE ?", query, query)
 	end
 
 end
