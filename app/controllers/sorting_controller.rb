bc4bf32808120e605fbea0a5bc921ebd2d6f08b3
mmaiatico <maiatico@gmail.com>
2015-08-17 15:23:04 -0400

    Style fixes for tables

--- app/controllers/sorting_controller.rb
+++ app/controllers/sorting_controller.rb
@@ -1,7 +1,8 @@
 class SortingController < ApplicationController
 
 	def index
-		@states = State.paginate(page: 1, per_page: State.count).order((params[:sort] || "name") + " " + (params[:dir] || "asc"))
+		@states = State.paginate(page: 1, per_page: State.count)
+							.order((params[:sort] || "name") + " " + (params[:dir] || "asc"))
 	end
 
 end
