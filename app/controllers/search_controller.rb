2922adad33b27a5b39c17508af831d5791fc6ddb
Eddie <epotocko@gmail.com>
2015-08-20 14:20:41 -0400

    UI fixes for grapple demo

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,7 @@ class SearchController < ApplicationController
 	def index
 		params[:query] ||= ""
 		query = "#{params[:query].downcase}%"
-		@states = State.paginate(page: params[:page] || 1, per_page: 10)
-							.where("LOWER(name) LIKE ? OR LOWER(code) LIKE ?", query, query)
+		@states = State.where("name LIKE ? OR code LIKE ?", query, query)
 	end
 
 end
