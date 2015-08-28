b3e5370ee51cfb9c54376ac823dfd9ee1a664572
Eddie <epotocko@gmail.com>
2015-08-28 16:58:58 -0400

    Changing production environment to serve assets raw

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,21 +19,19 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.assets.debug = true
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  #config.serve_static_assets = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+ # config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  #config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
